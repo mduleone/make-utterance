3633136c4662bc1a46ed58ed15e94a51537d8f0d
Matt DuLeone <Matt@Matts-MBP.home>
2015-12-06 02:39:25 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# About
+
+This is a very basic tool to make creating utterance strings for the Amazon Echo easier. Create utterances with variables that accept up to 22 words!
+
+# Usage
+
+Downlaod this repository. In the same directory, run the following command:
+```
+node index.js intentName utteranceString fileName
+```
+In the `utteranceString` string, place the identifier `%'max words''variable name'` where you want your variables
+
+# Sample
+
+```
+node index.js "Dictionary" "define %8term" "utterances.txt"
+```
+creates
+```
+utterances.txt:
+Dictionary define {this|term}
+Dictionary define {this is|term}
+Dictionary define {this is a|term}
+Dictionary define {this is a long|term}
+Dictionary define {this is a long sentence|term}
+Dictionary define {this is a long sentence that|term}
+Dictionary define {this is a long sentence that gets|term}
+Dictionary define {this is a long sentence that gets longer|term}
+```
+Running the command with a new utterance but specifying the same file
+```
+node index.js "Dictionary" "please define %8term" "utterances.txt"
+```
+appends to the file
+```
+utterances.txt:
+Dictionary define {this|term}
+Dictionary define {this is|term}
+Dictionary define {this is a|term}
+Dictionary define {this is a long|term}
+Dictionary define {this is a long sentence|term}
+Dictionary define {this is a long sentence that|term}
+Dictionary define {this is a long sentence that gets|term}
+Dictionary define {this is a long sentence that gets longer|term}
+Dictionary please define {this|term}
+Dictionary please define {this is|term}
+Dictionary please define {this is a|term}
+Dictionary please define {this is a long|term}
+Dictionary please define {this is a long sentence|term}
+Dictionary please define {this is a long sentence that|term}
+Dictionary please define {this is a long sentence that gets|term}
+Dictionary please define {this is a long sentence that gets longer|term}
+```
+
+You can include multiple variables in one utterance, so you're not limited to simple sentences
+
+```
+node index.js "RobinHood" "Steal from %4prince and give to %4pauper" "robinhood.txt"
+```
+Creates
+```
+RobinHood Steal from {this|prince} and give to {this|pauper}
+RobinHood Steal from {this|prince} and give to {this is|pauper}
+RobinHood Steal from {this|prince} and give to {this is a|pauper}
+RobinHood Steal from {this|prince} and give to {this is a long|pauper}
+RobinHood Steal from {this is|prince} and give to {this|pauper}
+RobinHood Steal from {this is|prince} and give to {this is|pauper}
+RobinHood Steal from {this is|prince} and give to {this is a|pauper}
+RobinHood Steal from {this is|prince} and give to {this is a long|pauper}
+RobinHood Steal from {this is a|prince} and give to {this|pauper}
+RobinHood Steal from {this is a|prince} and give to {this is|pauper}
+RobinHood Steal from {this is a|prince} and give to {this is a|pauper}
+RobinHood Steal from {this is a|prince} and give to {this is a long|pauper}
+RobinHood Steal from {this is a long|prince} and give to {this|pauper}
+RobinHood Steal from {this is a long|prince} and give to {this is|pauper}
+RobinHood Steal from {this is a long|prince} and give to {this is a|pauper}
+RobinHood Steal from {this is a long|prince} and give to {this is a long|pauper}
+```
---
+++ index.js
@@ -0,0 +1,154 @@
+/*
+this
+this is
+this is a
+this is a long
+this is a long sentence
+this is a long sentence that
+this is a long sentence that gets
+this is a long sentence that gets longer
+this is a long sentence that gets longer still
+this is a long sentence that gets longer still because
+this is a long sentence that gets longer still because I
+this is a long sentence that gets longer still because I do
+this is a long sentence that gets longer still because I do not
+this is a long sentence that gets longer still because I do not want
+this is a long sentence that gets longer still because I do not want to
+this is a long sentence that gets longer still because I do not want to repeat
+this is a long sentence that gets longer still because I do not want to repeat words
+this is a long sentence that gets longer still because I do not want to repeat words if
+this is a long sentence that gets longer still because I do not want to repeat words if it
+this is a long sentence that gets longer still because I do not want to repeat words if it can
+this is a long sentence that gets longer still because I do not want to repeat words if it can be
+this is a long sentence that gets longer still because I do not want to repeat words if it can be avoided
+*/
+
+var fs = require('fs');
+
+var words = [
+    "this",
+    "this is",
+    "this is a",
+    "this is a long",
+    "this is a long sentence",
+    "this is a long sentence that",
+    "this is a long sentence that gets",
+    "this is a long sentence that gets longer",
+    "this is a long sentence that gets longer still",
+    "this is a long sentence that gets longer still because",
+    "this is a long sentence that gets longer still because I",
+    "this is a long sentence that gets longer still because I do",
+    "this is a long sentence that gets longer still because I do not",
+    "this is a long sentence that gets longer still because I do not want",
+    "this is a long sentence that gets longer still because I do not want to",
+    "this is a long sentence that gets longer still because I do not want to repeat",
+    "this is a long sentence that gets longer still because I do not want to repeat words",
+    "this is a long sentence that gets longer still because I do not want to repeat words if",
+    "this is a long sentence that gets longer still because I do not want to repeat words if it",
+    "this is a long sentence that gets longer still because I do not want to repeat words if it can",
+    "this is a long sentence that gets longer still because I do not want to repeat words if it can be",
+    "this is a long sentence that gets longer still because I do not want to repeat words if it can be avoided",
+]
+
+function createUtterance (intentName, utterance, fileName) {
+
+    utterance = utterance.trim();
+    var splits = splitUtterance(utterance);
+
+    var outputArray = buildOutput(splits);
+
+    var output = '';
+    for (var i in outputArray) {
+        output += '' + intentName + ' ' + outputArray[i] + '\n';
+    }
+    fs.appendFile(fileName, output, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
+
+function buildOutput (splits) {
+    var output = [''];
+
+    for (var i in splits) {
+        output = stitch(output, splits[i]);
+    }
+
+    return output;
+}
+
+function stitch (fronts, next) {
+    var newFronts = [];
+    var current = '';
+
+    if (next.type == "text") {
+        for (var i in fronts) {
+            newFronts.push(fronts[i] + next.text);
+        }
+    } else if (next.type == "var") {
+        newFronts = stitchVar(fronts, next);
+    }
+
+    return newFronts;
+}
+
+function stitchVar (fronts, term) {
+    var newFronts = [];
+
+    for (var front in fronts) {
+        var current = fronts[front];
+        for (var i = 0; i < term.num; i++) {
+            var curr = current;
+            curr += "{" + words[i] + "|" + term.name + "}";
+            newFronts.push(curr);
+        }
+    }
+
+    return newFronts;
+}
+
+function splitUtterance (utterance) {
+    var pattern = /(.*)\%(\d+)([^\s]+)(.*)/;
+
+    var utterances = [];
+    var cont = true;
+    var remaining = utterance;
+    while (cont) {
+        var matches = remaining.match(pattern);
+        var uttText = {};
+        var uttVar = {};
+        if (matches === null) {
+            uttText.type = "text";
+            uttText.text = remaining;
+            utterances.unshift(uttText);
+            cont = false;
+            continue;
+        }
+        remaining = matches[1];
+
+        uttVar.type = "var"
+        uttVar.num = Number.parseInt(matches[2]);
+        uttVar.name = matches[3];
+
+        uttText.type = "text"
+        uttText.text = matches[4];
+        utterances.unshift(uttVar, uttText);
+    }
+
+    return utterances;
+}
+
+module.exports = {
+    createUtterance: createUtterance,
+    splitUtterance: splitUtterance,
+    words: words,
+    stitch: stitch,
+    stitchVar: stitchVar,
+    buildOutput: buildOutput,
+}
+
+/**
+ * Using it!
+ */
+createUtterance(process.argv[2], process.argv[3], process.argv[4])
